a15dd5ce6e9551d49b8621d7c897d1c54d94186b
khsung <khsung0@nate.com>
2021-03-25 01:27:49 +0900

    이미지 주소 크롤링
    파일 하나에 대해 완성

--- selenium/get_url.py
+++ selenium/get_url.py
@@ -4,40 +4,27 @@ import pandas as pd
 import time
 import urllib.request
 
-column_num=13
-                    #파일 이름                                     가져올 행
-df=pd.read_excel('./Book_DB/pulp-books.xls',sheet_name='Worksheet',usecols=[column_num])
-#df=pd.read_excel('./Book_DB/test.xls',sheet_name='Sheet1')
-print(len(df))
-#print(df.values)
-for i in range(len(df)):
+file_names=['banbi','bir','goldenbough','minumin','minumsa','panmidong','pulp','semicolon']
+column_num=[13,12,13,13,13,13,13,13]     #웹주소있는 컬럼 모음
+index=6
+                    #파일 이름                                   시트선택              가져올 행
+df=pd.read_excel('./Book_DB/'+file_names[index]+'-books.xls',sheet_name='Worksheet')
 
-    print(df.values[i][0])
+url_list=[]
 for i in range(len(df)):
     if i==0:
         driver = webdriver.Chrome()
-    driver.get(df.values[i][0])
-    time.sleep(1)
+    driver.get(df.values[i][column_num[index]])         #해당 주소로 이동
+    #로딩화면 대기 시간
+    driver.implicitly_wait(1)
+    img=driver.find_element_by_css_selector('#single-book > div.header.clearfix > div.left > div.cover.inner-border > img')
+    #이미지 주소 얻어오기
+    img_url=img.get_attribute("src")
+    #이미지 주소 리스트 형태로 저장
+    url_list.append(img_url)
+   
+#이미지 url 컬럼 추가 후 엑셀 파일로 저장
+df['이미지 주소']=img_url
+df.to_excel('./Book_DB/'+file_names[index]+'-books1.xls', index=False)
 
-
-driver.close()
-    #print(df.values[i][0])
-
-
-#driver = webdriver.Chrome()
-
-# driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("강아지")
-# elem.send_keys(Keys.RETURN)
-# images=driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-# cnt=1
-# for image in images:
-#     image.click()
-#     image_url=image.get_attribute("src")
-#     urllib.request.urlretrieve(image_url, str(cnt)+".jpg")
-#     if cnt==5:
-#         break
-#     cnt+=1
-
-#driver.close()      브라우저 종료
+driver.close()      #브라우저 종료
