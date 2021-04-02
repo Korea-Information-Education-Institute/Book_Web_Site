47a1a77005f29bd6f3c790e0984b43a3d05a97bc
khsung <khsung0@nate.com>
2021-04-03 00:32:05 +0900

    경로 추가

--- html/mypage.php
+++ html/mypage.php
@@ -4,17 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/basic.css">
-    <link rel="stylesheet" href="../css/index.css?after"> <!--  캐시문제로 서버에서 외부스타일시트 변경사항이 적용안되어 임의의 문자열삽입 -->
+    <link rel="stylesheet" href="../css/index.css">
     <title>Document</title>
 </head>
 
 <?php
+//세션에 user_id 변수가 있을 경우 로그인 상태로 판단
   if( isset( $_SESSION[ 'user_id' ] ) ) {
     echo "<style>#logined{display:inline-block;}</style>";
     echo "<style>#logouted{display:none;}</style>";
+//변수가 없으면 로그아웃된 상태로 판단
   }else{
     echo "<style>#logined{display:none;}</style>";
     echo "<style>#logouted{display:inline-block;}</style>";
+    echo "<script>alert('로그인을 하십시오.')</script>";
+    echo "<script>location.href='./login.html';</script>";
   }
 ?>
 
@@ -38,7 +42,7 @@
                     <button type="button" onClick="location.href='./register.html'">회원가입</button>
                 </div>
 	            <div class="login_menu" id="logined">
-                    <?php
+                    	    <?php
 		            echo $_SESSION['user_name']."님 환영합니다.";
 	                ?>
                     <button type="button" onClick="location.href='./logout.php'">로그아웃</button>
@@ -51,32 +55,30 @@
         </div>
         <div class="container">
             <div class="container_inner">
-                <div class="moving">
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                    </ul>
-                </div>
-                <div class="box">
-                   <ul class="section">
-                       <li>책1</li>
-                       <li>책1</li>
-                       <li>책1</li>
-                   </ul>
-                   <ul class="section">
-                       <li>책1</li>
-                       <li>책1</li>
-                       <li>책1</li>
-                   </ul>
-                   <ul class="section">
-                       <li>책1</li>
-                       <li>책1</li>
-                       <li>책1</li>
-                   </ul>
-                </div>
+                <?php
+		if( isset( $_SESSION[ 'user_id' ] ) ) {
+			if(include('./dbconnect.php')){
+				$user_id=$_SESSION[ 'user_id' ];
+				$sql="SELECT * FROM user WHERE user_id='$user_id'";
+				$result=mysqli_query($conn, $sql);
+
+				//결과는 1행밖에 없기 때문에 while문은 한번만 반복
+				while($row=mysqli_fetch_array($result)){
+					echo "<p>아이디 : {$row['user_id']}</p>";
+					echo "<p>닉네임 : {$row['user_nickname']}</p>";
+					echo "<p>이름 : {$row['user_name']}</p>";
+					echo "<p>생일 : {$row['user_birth']}</p>";
+					if ($row['user_gender']=="m"){
+						echo "<p>성별 : 남성</p>";
+					}else{
+						echo "<p>성별 : 여성</p>";
+					}
+				}
+			}
+			mysqli_close($conn);
+		}
+	    ?>
+	    <button type="button" onClick="location.href='./change_pw.php'">비밀번호 변경</button>
             </div>
         </div>
         <div class="footer">
@@ -86,5 +88,4 @@
         </div>
     </div>
 </body>
-
 </html>
