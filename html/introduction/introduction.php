53c2d1c2663295b2a6107c6f6625c36a6ff69b5a
khsung <khsung0@nate.com>
2021-04-08 10:14:05 +0900

    로그인 상황 변경

--- html/introduction/introduction.php
+++ html/introduction/introduction.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="ko">
+<?php session_start(); ?>
 <head>
     <meta charset="UTF-8">
     <title>소개페이지</title>
     <link rel="stylesheet" href="../../css/basic.css">
     <link rel="stylesheet" href="../../css/index.css">
     
+    <?php
+        if( isset( $_SESSION[ 'user_id' ] ) ) {
+            echo "<style>#logined{display:inline-block;}</style>";
+            echo "<style>#logouted{display:none;}</style>";
+        }else{
+            echo "<style>#logined{display:none;}</style>";
+        echo "<style>#logouted{display:inline-block;}</style>";
+        }
+    ?>
+
     <style>
         .container{
             width: 980px;
@@ -58,15 +69,21 @@
                 </div>
                 <div class="header_nav">
                     <ul>
-                        <li><a href="./introduction.html">소개페이지</a></li>
+                        <li><a href="./introduction.php">소개페이지</a></li>
                         <li><a href="">책분야</a></li>
                         <li><a href="">로드맵</a></li>
                         <li><a href="../mypage.php">마이페이지</a></li>
                     </ul>
                 </div>
-                <div class="login_menu">
-                    <button type="button">로그인</button>
-                    <button type="button">회원가입</button>
+                <div class="login_menu" id="logouted">
+                    <button type="button" onClick="location.href='./login.html'">로그인</button>
+                    <button type="button" onClick="location.href='./register.html'">회원가입</button>
+                </div>
+	            <div class="login_menu" id="logined">
+                    <?php
+		            echo $_SESSION['user_name']."님 환영합니다.";
+	                ?>
+                    <button type="button" onClick="location.href='./logout.php'">로그아웃</button>
                 </div>
                 <div class="search">
                     <input type="text">
@@ -85,7 +102,7 @@
          
          <div class="containerNav">
              <ul>
-                 <li><a href="./introduction.html">사이트 소개</a></li>
+                 <li><a href="./introduction.php">사이트 소개</a></li>
                  <li><a href="./method.html">사이트 이용방법</a></li>
                  <li><a href="./directionality.html">사이트의 방향성</a></li>
                  <li><a href="./faq.html">자주하는 질문</a></li>
