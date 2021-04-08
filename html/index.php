ac7311cb462298f337a26d15e3dd5c21e4f198c3
seongjin231 <52709744+seongjin231@users.noreply.github.com>
2021-04-08 15:03:11 +0900

    Update index.php

--- html/index.php
+++ html/index.php
@@ -20,36 +20,9 @@
 
 <body>
     <div class="wrap">
-        <div class="header">
-            <div class="header_inner">
-                <div class="header_logo">
-                <a href="./index.php"><img src="#" alt="로고"></a>
-                </div>
-                <div class="header_nav">
-                    <ul>
-                        <li><a href="./introduction/introduction.php">소개페이지</a></li>
-                        <li><a href="./book_list.php">책분야</a></li>
-                        <li><a href="">로드맵</a></li>
-                        <li><a href="./mypage.php">마이페이지</a></li>
-                    </ul>
-                </div>
-                <div class="login_menu" id="logouted">
-                    <button type="button" onClick="location.href='./login.html'">로그인</button>
-                    <button type="button" onClick="location.href='./register.html'">회원가입</button>
-                </div>
-	            <div class="login_menu" id="logined">
-                    <?php
-		            echo $_SESSION['user_name']."님 환영합니다.";
-	                ?>
-                    <button type="button" onClick="location.href='./logout.php'">로그아웃</button>
-                </div>
-                <div class="search">
-                    <?php 
-                        include './search.php';
-                    ?>
-                </div>
-            </div>
-        </div>
+        <?php 
+            include './header.php';
+        ?>
         <div class="container">
             <div class="container_inner">
                 <div class="moving">
@@ -80,11 +53,9 @@
                 </div>
             </div>
         </div>
-        <div class="footer">
-            <div class="footer_inner">
-                <p>푸터입니다.</p>
-            </div>
-        </div>
+        <?php 
+            include "./footer.php";
+        ?>
     </div>
     <script type="text/javascript" src="../javascript/search.js"></script>
 </body>
