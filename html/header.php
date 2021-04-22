9d8c312cfad8d511164a4ff0f7ef03fb7ff99220
khsung <khsung0@nate.com>
2021-04-22 11:30:34 +0900

    login.css

--- html/header.php
+++ html/header.php
@@ -16,37 +16,37 @@
             $_SESSION['book_genre']="문학/시";
         }
         ?>
-        location.href="http://khsung0.dothome.co.kr/html/book_list.php";
+        location.href="http://khsung1.dothome.co.kr/html/book_list.php";
     }
 </script>
 
 <div class="header">
     <div class="header_inner">
         <div class="header_logo">
-        <a href="http://khsung0.dothome.co.kr/html/index.php"><img src="#" alt="로고"></a>
+        <a href="http://khsung1.dothome.co.kr/html/index.php"><img src="#" alt="로고"></a>
         </div>
         <div class="header_nav">
 
 	<!-- introduction폴더에도 적용하기 위해 절대경로 설정 -->
             <ul>
-                <li><a href="http://khsung0.dothome.co.kr/html/introduction/introduction.php">소개페이지</a></li>
+                <li><a href="http://khsung1.dothome.co.kr/html/introduction/introduction.php">소개페이지</a></li>
                 <li><a href="javascript:book_list();" >책분야</a></li>
                 <li><a href="">로드맵</a></li>
-                <li><a href="http://khsung0.dothome.co.kr/html/mypage.php">마이페이지</a></li>
+                <li><a href="http://khsung1.dothome.co.kr/html/mypage.php">마이페이지</a></li>
             </ul>
         </div>
         <div class="login_menu" id="logouted">
-            <button type="button" onClick="location.href='http://khsung0.dothome.co.kr/html/login.php'">로그인</button>
-            <button type="button" onClick="location.href='http://khsung0.dothome.co.kr/html/register.php'">회원가입</button>
+            <button type="button" id="login_btn" onClick="location.href='http://khsung1.dothome.co.kr/html/login.php'">로그인</button>
+            <button type="button" id="register_btn" onClick="location.href='http://khsung1.dothome.co.kr/html/register.php'">회원가입</button>
         </div>
         <div class="login_menu" id="logined">
             <?php
                 echo $_SESSION['user_name']."님 환영합니다.";
             ?>
-            <button type="button" onClick="location.href='http://khsung0.dothome.co.kr/html/logout.php'">로그아웃</button>
+            <button type="button" onClick="location.href='http://khsung1.dothome.co.kr/html/logout.php'">로그아웃</button>
         </div>
         <div class="search">
-            <form name="search" method="post" action="http://khsung0.dothome.co.kr/html/search.php" onsubmit="return search_check()">
+            <form name="search" method="post" action="http://khsung1.dothome.co.kr/html/search.php" onsubmit="return search_check()">
                 <input type="text" name="search_var">
                 <button type="submit">검색</button>
             </form>
