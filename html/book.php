2a31d2a99aec5035b12d839780a28519721851d9
khsung <khsung0@nate.com>
2021-04-22 10:36:17 +0900

    Revert "Update book.php"
    This reverts commit b6162c442945dbe26b1a56dae190e4ef666e7751.

--- html/book.php
+++ html/book.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../css/book.css">
-    <?php include "./head.php"; ?>
+    <link rel="stylesheet" href="../css/basic.css">
+    <link rel="stylesheet" href="../css/index.css?after">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
