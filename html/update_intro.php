3ac43baa4e1328f91bbcaa0a337e412b04215252
khsung <khsung0@nate.com>
2021-04-22 11:52:08 +0900

    로그인 유저만 편집 가능 수정

--- html/update_intro.php
+++ html/update_intro.php
@@ -3,9 +3,15 @@ header("Content-Type: text/html; charset=UTF-8");
     try {
 		$intro = $_POST['intro'];
         $title = $_POST['title'];
-
-		if(!$intro||!$title) {
-			throw new exception('오류');
+		$type = $_POST['type'];
+		$user_id = $_POST['user_id'];
+		if($type=='edit'){
+			if($user_id==''){
+				$result['success']	= false;
+				$result['msg']	= "로그인 사용자만 가능합니다.";
+			}else{
+				$result['success']	= true;
+			}
 		}else{
 			$conn = mysqli_connect("localhost", "khsung0", "gmltjd1!" , "khsung0");
 			$sql = "SELECT * FROM book where book_title='$title'";
@@ -22,7 +28,7 @@ header("Content-Type: text/html; charset=UTF-8");
 				$result['success_msg']	= "소개글을 업데이트 하였습니다.";
 				}
 			}
-        }
+		}
 	} catch(exception $e) {		
 		$result['success']	= false;
 		$result['msg']		= $e->getMessage();
