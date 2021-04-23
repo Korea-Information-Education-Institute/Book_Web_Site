130e52469093634dd588827576a61e91f1b2148b
khsung <khsung0@nate.com>
2021-04-23 16:52:58 +0900

    favicon upload

--- html/head.php
+++ html/head.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="../css/main.css">
 <link rel="stylesheet" href="../css/index.css">
+<link rel="shortcut icon" href="http://khsung0.dothome.co.kr/img/favicon.png">
+<link rel="icon" href="http://khsung0.dothome.co.kr/img/favicon.png">
