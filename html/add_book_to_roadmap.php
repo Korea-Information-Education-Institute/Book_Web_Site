d3c7f006e0336fba10255a90cc17928f5c90f2eb
khsung <khsung0@nate.com>
2021-05-03 18:18:27 +0900

    Update add_book_to_roadmap.php

--- html/add_book_to_roadmap.php
+++ html/add_book_to_roadmap.php
@@ -11,9 +11,17 @@
     function add_book(){
         var  url_parse=window.location.href.split('?');
         if(url_parse[1]=='book1'){
+            opener.document.getElementById("square_book1").removeAttribute('href');
+            opener.document.getElementById("square_book1").removeAttribute('onclick');
             opener.document.getElementById("square_book2").style.display="block";
         }else if(url_parse[1]=='book2'){
+            opener.document.getElementById("square_book2").removeAttribute('href');
+            opener.document.getElementById("square_book2").removeAttribute('onclick');
             opener.document.getElementById("square_book3").style.display="block";
+            
+        }else{
+            opener.document.getElementById("square_book3").removeAttribute('href');
+            opener.document.getElementById("square_book3").removeAttribute('onclick');
         }
         window.close();
         // temp.src="http://image.dongascience.com/Photo/2020/03/5bddba7b6574b95d37b6079c199d7101.jpg";
